d3f9bf86c4b379ec30d4fd97c0360ad45b9bfab9
Cristhian Vera <cristianmario092190@gmail.com>
2024-05-27 17:38:51 -0500

    feat: add hook ref in explicit in fox.tsx

--- pages/Fox.tsx
+++ pages/Fox.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 //definir aquí los prop
 type Props = {
   imgFox: string;
@@ -7,8 +9,12 @@ type Props = {
 //función explicita que devuelve un JSX (devuelve un html)
 //los children de tipo Props
 const Fox = ({ imgFox }: Props): JSX.Element => {
+  //ref para referenciar un valor en react
+  const node = useRef<HTMLImageElement>(null);
+
   return (
     <img
+      ref={node}
       className="rounded-lg mx-auto w-[600px] h-auto border border-black"
       src={imgFox}
     />
