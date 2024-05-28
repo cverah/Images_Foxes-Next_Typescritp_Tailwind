a15b17e2defa23938e16e9455e9def736530dd36
Cristhian Vera <cristianmario092190@gmail.com>
2024-05-28 18:08:19 -0500

    feat: lazy loading with IntersectionObserver optimize img

--- pages/Fox.tsx
+++ pages/Fox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 //definir aquí los prop
 type Props = {
@@ -11,12 +11,44 @@ type Props = {
 const Fox = ({ imgFox }: Props): JSX.Element => {
   //ref para referenciar un valor en react
   const node = useRef<HTMLImageElement>(null);
+  //inicializar con cuadro transparente
+  const [srcImage, setSrcImage] = useState<string>(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+
+  //agregamos observador
+  useEffect(() => {
+    //iniciar observador
+    //Intersection observer recibe unas entradas que es un array
+    // tipo void significa que es una función que no devuelve nada
+    const observe = new IntersectionObserver((entries): void => {
+      entries.forEach((entry): void => {
+        //on intersection cuando la imagen es visible
+        if (entry.isIntersecting) {
+          setSrcImage(imgFox);
+        }
+      });
+    });
+    //nodo observador
+    //aquí se puede poner asi con un ! al final ya que dirá que no es necesario validar ya que siempre existirá
+    // observe.observe(node.current!);
+    // pero es mejor poner asi
+    if (node.current) {
+      observe.observe(node.current);
+    }
+    //desconectar
+    return (): void => {
+      observe.disconnect();
+    };
+  }, [imgFox]);
 
   return (
     <img
       ref={node}
-      className="rounded-lg mx-auto w-[600px] h-auto border border-black"
-      src={imgFox}
+      //bg-slate-500 fondo gris
+      className="rounded-lg mx-auto w-[320px] h-auto border border-black bg-slate-500"
+      //inicializar la imagen con el cuadro gris
+      src={srcImage}
     />
   );
 };
